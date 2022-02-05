ab74ca848d19844baa66f6d562b037c9b90ffebf
allpavel <allpavel@yandex.ru>
2022-02-05 20:38:55 +0300

    Designed a navigation bar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <h2>This is a Footer</h2>
+        <footer>
+            <h2>This is a Footer</h2>
+        </footer>
     )
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import '../../assets/css/normalize.css'
+import '../../assets/css/global.css';
 
 export default function Layout({ children }) {
     return (
-        <>
+        <section className='page_wrapper'>
             <Navbar />
             {children}
             <Footer />
-        </>
+        </section>
     )
 };
